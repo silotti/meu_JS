991ed33b04da8bf79db17d1f67818b556cad15b7
silotti <silotti@gmail.com>
2026-03-19 11:06:44 -0300

    Incorporar JS direto na página_Soma

--- script_Calculadora.js
+++ script_Calculadora.js
@@ -1,4 +1,5 @@
-//Arquivo JS
+//Arquivo JS Externo para a Calculadora
+//Funções em JavaScript para realizar as operações matemáticas
 function somar(a,b) {return a + b;}
 function subtrair(a,b) {return a - b;}
 function multiplicar(a,b) {return a * b;}
