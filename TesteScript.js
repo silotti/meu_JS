2a21b8cb1922e14cf37081a3880eb40e4e42f647
silotti <silotti@gmail.com>
2026-03-20 16:04:42 -0300

    Vetores

---
+++ TesteScript.js
@@ -0,0 +1,8 @@
+//Teste de script
+let somaTotal=0;
+const qtdeInteracoes=50;
+
+for (let i=qtdeInteracoes; i>=1; i--) {
+    somaTotal += i; // somaTotal = somaTotal + i;
+}
+console.log('Soma Total:',(somaTotal));
